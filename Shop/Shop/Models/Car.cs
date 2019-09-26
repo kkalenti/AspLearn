8d5b62ccc29034a69de63f960f646d580d79f8e6
Калентьев Константин Анатольевич <kalentyev_ka@tomsk.lemz.ru>
2019-09-26 16:45:12 +0700

    Project init

---
+++ Shop/Shop/Models/Car.cs
@@ -0,0 +1,45 @@
+﻿namespace Shop.Models
+{
+    public class Car
+    {
+        /// <summary>
+        /// Id машины
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название машины
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Краткое описание
+        /// </summary>
+        public string ShortDescription { get; set; }
+        
+        /// <summary>
+        /// Полное описание
+        /// </summary>
+        public string FullDescription { get; set; }
+
+        /// <summary>
+        /// Краткое описание
+        /// </summary>
+        public string Image { get; set; }
+
+
+
+        public ushort Price { get; set; }
+
+        public bool IsFavourite { get; set; }
+
+        public int AvailableCount { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+
+
+    }
+}
---
+++ Shop/Shop/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿namespace Shop.Models
+{
+    public class Category
+    {
+        /// <summary>
+        /// Id категории
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название категории
+        /// </summary>
+        public string CategoryName { get; set; }
+
+        /// <summary>
+        /// Описание категории
+        /// </summary>
+        public string Description { get; set; }
+
+       //public int Id { get; set; }
+
+
+    }
+}
